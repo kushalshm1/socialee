4468140fb0597eaa50b5ddc926960dfe48ffadc6
imuskanshukla <shuklamuskan2703@gmail.com>
2021-05-12 23:33:35 +0530

    one-page

---
+++ frontend/static/js/main.js
@@ -0,0 +1,21 @@
+let fadeOutTime = 300;
+let fadeInTime = 1500;
+$(document).ready(function () {
+    $('#feed-page').hide();
+    $('#login-page').show();
+    $('#profile-page').hide();
+    $('.login-button').click(function () {
+            $('#login-page').fadeOut(fadeOutTime);
+            $('#feed-page').fadeIn(fadeInTime);
+    });
+    $('.profile').click(function(){
+        $('#login-page').fadeOut(fadeOutTime);
+        $('#feed-page').fadeOut(fadeOutTime);
+        $('#profile-page').fadeIn(fadeInTime);
+    });
+    $('.home').click(function(){
+        $('#login-page').fadeOut(fadeOutTime);
+        $('#feed-page').fadeIn(fadeInTime);
+        $('#profile-page').fadeOut(fadeOutTime);
+    });
+});
