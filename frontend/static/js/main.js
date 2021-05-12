87080c6ec55904f4911135acd319124ec54a448f
imuskanshukla <shuklamuskan2703@gmail.com>
2021-05-12 23:43:50 +0530

    final UI

--- frontend/static/js/main.js
+++ frontend/static/js/main.js
@@ -1,3 +1,8 @@
+<<<<<<< HEAD
+=======
+// Animating login div
+let pages = ['#login-page','#feed-page','#profile-page']
+>>>>>>> 573b1e93d2f867a2ecc53e7b8ad45ac359bd18d2
 let fadeOutTime = 300;
 let fadeInTime = 1500;
 $(document).ready(function () {
@@ -18,4 +23,9 @@ $(document).ready(function () {
         $('#feed-page').fadeIn(fadeInTime);
         $('#profile-page').fadeOut(fadeOutTime);
     });
+<<<<<<< HEAD
 });
+=======
+});
+
+>>>>>>> 573b1e93d2f867a2ecc53e7b8ad45ac359bd18d2
